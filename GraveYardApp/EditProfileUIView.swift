f941f0a0c56c66eb7d0533263b60829cf7224947
Tyler Donohue <donohue_tyler@yahoo.com>
2019-10-16 12:07:36 -0600

    Creating and linking Created .swift files and linked them with the correct subclass to the MainStoryboard

---
+++ GraveYardApp/EditProfileUIView.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileUIView.swift
+//  GraveYardApp
+//
+//  Created by Tyler Donohue on 10/16/19.
+//  Copyright © 2019 Tyler Donohue. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileUIView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
